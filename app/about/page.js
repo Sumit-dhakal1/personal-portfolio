cb06101f75386f831a73fc12702c026c4b11f401
sumit dhakal <sumudhakal94@gmail.com>
2026-06-20 02:18:58 +0545

    enhance UI

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,5 @@
 export default function About() {
-  const skills = [
+const skills = [
     "React",
     "Next.js",
     "Node.js",
@@ -11,172 +11,171 @@ export default function About() {
     "JavaScript",
     "TypeScript",
     "MYSQL",
-  ];
+];
 
-  return (
+return (
     <>
-      <main className=" bg-green-100 ">
-        <section className="relative overflow-hidden px-100 py-10 sm:px-10 lg:px-10">
-          <div className="absolute left-1/2 top-0 h-[520px] w-[520px] bg-sky-500/1 blur-3xl" />
-          <div className="absolute right-11 top-240 h-500 bg-violet-200/20 blur-3xl" />
+    <main className="about">
+        <section className="relative overflow-hidden px-100 py-30 bg-white-200  sm:px-10 lg:px-7">
+        <div className="absolute right-11 top-240 h-500  blur-3xl" />
 
-          <div className="relative mx-auto max-w-10xl  gap-10 lg:flex-row ">
-            <div className="space-y-7 lg:w-1/1">
-              <h1 className="text-3xl  tracking-tight text-grey text-1xl">
+        <div className="relative mx-auto max-w-10xl  gap-1 lg:flex-row ">
+            <div className="space-y-5 lg:w-1/1">
+            <h1 className="text-4xl tracking-tight text-black text-1xl">
                 I build modern webpage, application potery experiences with
                 elegant motion and strong logic.
-              </h1>
-              <p className="max-w-xl text-slate-200 sm:text-lg leading-9">
+            </h1>
+        <p className="max-w-xl text-black-200 sm:text-lg leading-9  ">
                 I am a passionate fullstack developer blending creative UI
                 design with robust backend systems. My work focuses on reliable
                 applications that feel delightful to use.
-              </p>
+            </p>
             </div>
 
-            <div className=" flex flex direction shadow-4xl shadow-slate-150/10 backdrop-blur-x10">
-              <div className="flex h-1 p-10 animate-float display text-white shadow-sky-10/5">
+            <div className=" flex flex direction shadow-4xl shadow-slate-150/10 ">
+            <div className="flex h-1 p-10 animate-float display text-black shadow-sky-10/5">
                 <div className="space-y-10 ">
-                  <h1 className="text-5xl font-semibold">
+                <h1 className="text-5xl font-semibold">
                     HI, iam software engineer
-                  </h1>
+                </h1>
                 </div>
-              </div>
+            </div>
 
-              <div className="mt-1 space-y-5 ">
-                <div className=" border border-white/20 bg-slate-950/100   p-5 ">
-                  <h3 className="text-lg font-semibold  text-white">Contact</h3>
-                  <p className="mt-5 text-white  flex flex-wrap leading-8">
+            <div className="space-y-2 ">
+                <div className=" border border-white/20 bg-slate-950/20   p-15 ">
+                <h3 className="text-lg font-semibold  text-white">Contact</h3>
+                <p className="mt-5 text-white  flex flex-wrap leading-8">
                     Itahari-04, Sunsari, Nepal
                     <br />
                     sumitdhakal51@gmail.com
                     <br />
                     +977-9827443797
-                  </p>
+                </p>
                 </div>
 
-                <div className=" p-10 bg-black h-200">
-                  <h3 className=" font-semibold text-2xl m-4 font-bold text-white ">
+                <div className=" p-10 bg-blue-300 p-15 h-200">
+                <h3 className=" font-semibold text-2xl m-4 font-bold text-white ">
                     What I building
-                  </h3>
+                </h3>
 
-                  <p className="mt-3  text-white  leading-7">
+                <p className="mt-3  text-white  leading-7">
                     Inventory management system <br />
                     vechile booking system
                     <br />
                     Anti-theft bike detication
                     <br />
                     CIVIC watch
-                  </p>
+                </p>
 
-                  <div className="text-white">
+                <div className="text-white">
                     <p className=" text-2xl m-4 font-bold text-white">
-                      Education
+                    Education
                     </p>
                     <h2 className=" mt-4 leading-6  text-white-400">
-                      {" "}
-                      secondary: merryland higher secondary school 2023 passout
-                      faculty- business management
+                    {" "}
+                    secondary: merryland higher secondary school 2023 passout
+                    faculty- business management
                     </h2>
 
                     <h2 className=" mt-4 leading-6  text-white-400">
-                      {" "}
-                      undergraduate: Itahari international college currently
-                      running/ faculty- BSC-HONS computing
+                    {" "}
+                    undergraduate: Itahari international college currently
+                    running/ faculty- BSC-HONS computing
                     </h2>
 
                     <p className="text-2xl m-4 font-bold text-white">
-                      Project and certification
+                    Project and certification
                     </p>
                     <p className="mt-4 text-white leading-7">
-                      COIT - college of information technology: computer
-                      operator
-                      <br />
-                      LOC - computer fundamentals, nteworking, hardware
-                      concepts, multi-media
+                    COIT - college of information technology: computer
+                    operator
+                    <br />
+                    LOC - computer fundamentals, nteworking, hardware
+                    concepts, multi-media
                     </p>
 
                     <p className="text-2xl m-4 font-bold text-white">
-                      Soft skills
+                    Soft skills
                     </p>
                     <p className="mt-4 text-white leading-7">
-                      communication <br />
-                      leadership <br />
-                      team work
-                      <br />
-                      punctuality
-                      <br />
+                    communication <br />
+                    leadership <br />
+                    team work
+                    <br />
+                    punctuality
+                    <br />
                     </p>
-                  </div>
                 </div>
-              </div>
+                </div>
             </div>
-            <p className=" uppercase text-5xl font-bold text-white  p-10 flex flex-wrap text-white-300">
-              Skills
+            </div>
+            <p className=" uppercase text-5xl font-bold text-black  p-10 flex flex-wrap text-black-300">
+            Skills
             </p>
             <div className="mt-1  flex flex-wrap gap-5">
-              {skills.map((skill) => (
+            {skills.map((skill) => (
                 <span
-                  key={skill}
-                  className="rounded-1x2 bg-white/5 px-4 py-2 text-sm text-slate-200 ring-1 ring-white/10 transition hover:bg-sky-700/100 hover:text-white"
+                key={skill}
+                className="rounded-1x2 bg-black/5 px-4 py-2 text-sm text-black-200 ring-1 ring-white/10 transition hover:bg-sky-700/100 hover:text-white"
                 >
-                  {skill}
+                {skill}
                 </span>
-              ))}
+            ))}
             </div>
-          </div>
+        </div>
         </section>
 
         <section className=" g:flex-row h-100 px-1">
-          <div className=" bg-slate-600 p-3   ">
+        <div className=" bg-blue-400 p-3 h-100  ">
             <h2 className="text-5xl font-bold text-white text-white-300">
-              My approach
+            My approach
             </h2>
 
-            <div className="mt-5 grid gap-2 sm:grid-cols-5">
-              <div className="  bg-slate-950 p-5">
+            <div className="mt-5 grid gap-2 sm:grid-cols-5 ">
+            <div className="  bg-orange-500 p-5 rounded-[1.1rem]">
                 <h3 className="font-semibold text-white">Design</h3>
                 <p className="mt-2 text-sm text-white leading-7">
-                  plan the system structure in Clean layouts, vibrant gradients
-                  subtle motion UI friendly.
+                plan the system structure in Clean layouts, vibrant gradients
+                subtle motion UI friendly.
                 </p>
-              </div>
-              <div className=" bg-slate-950 p-5">
+            </div>
+            <div className=" bg-orange-500 p-5 rounded-[1.1rem]">
                 <h3 className="font-semibold text-white">Development</h3>
                 <p className="mt-2 text-sm text-white leading-7">
-                  Trun the design into beautiful and Scalable code with modern
-                  frameworks.
+                Trun the design into beautiful and Scalable code with modern
+                frameworks.
                 </p>
-              </div>
+            </div>
 
-              <div className=" bg-slate-950 p-5">
+            <div className=" bg-orange-500 p-5 rounded-[1.1rem] ">
                 <h3 className="font-semibold text-white">Sclable</h3>
                 <p className="mt-2 text-sm text-white leading-7">
-                  build system that handle growing users in the future easily
-                  through Scalable code with modern frameworks and maintainable
-                  architecture.
-                </p>
-              </div>
+                build system that handle growing users in the future easily
+                through Scalable code with modern frameworks and maintainable
+                architecture.
+            </p>
+            </div>
 
-              <div className=" bg-slate-950 p-5">
+            <div className=" bg-orange-400 p-5 rounded-[1.1rem]">
                 <h3 className="font-semibold text-white">security</h3>
                 <p className="mt-2 text-sm text-white leading-7">
-                  protect the system along with data from threats, attracts, and
-                  unauthorize access through optimization a long with secure
-                  system design.
+                protect the system along with data from threats, attracts, and
+                unauthorize access through optimization a long with secure
+                system design.
                 </p>
-              </div>
+            </div>
 
-              <div className=" bg-slate-950 p-5">
+            <div className=" bg-orange-400 p-5 rounded-[1.1rem]">
                 <h3 className="font-semibold text-white">performance</h3>
                 <p className="mt-2 text-sm text-white leading-7">
-                  making the system efficiently and reliable for mass users
-                  through performance optimization in the system.
+                making the system efficiently and reliable for mass users
+                through performance optimization in the system.
                 </p>
-              </div>
             </div>
-          </div>
+            </div>
+        </div>
         </section>
-      </main>
+    </main>
     </>
-  )
+)
 };
--- app/components/Main.js
+++ app/components/Main.js
@@ -6,21 +6,19 @@ import React from "react"
 export default function Main() {
 
 
-
-
-
 return (
     <section id="main">    
 
 
-            <main className="min-h-screen w-full bg-green-700 px-6 py-16 text-white-900">
-    <div className="mx-auto flex max-w-6xl flex-col items-center justify-between gap-10 lg:flex-row lg:gap-16">
-        <div className="max-w-2xl space-y-6 ">
+            <main className="min-h-screen w-full bg-white-300 px-5 py-5 text-white-900">
+    <div className="mx-auto flex max-w-5xl  items-center justify-center gap-10 lg:flex-row lg:gap-50">
+        <div className="max-w-1xl space-y-10 ">
     
-        <h1 className="text-5xl font-semibold ">
-        SUMIT DAHKAL
+        <h1 className="text-5xl  font-bold ">
+        
+        Sumit Dhakal
         </h1>
-        <p className=" leading-5 text-white-100 text-lg">
+        <p className=" leading-7   text-lg">
             Aspring a fullstack devloper  with curiosity and contributing
         in the inovative projects.
         </p>
@@ -40,19 +38,22 @@ return (
         </div>
         </div>
 
-        <div className="relative flex w-full max-w-xl items-center justify-center">
-        <div className="pointer-events-none absolute inset-x-0 top-0 h-72 rounded-full bg-sky-20/50 blur-3xl" />
-        <div className=" rounded-[20rem] border border-slate-500 bg-black/10 p-1">
-            <Image
+
+        <div className="relative flex w-full max-w-1xl items-center justify-center">
+                    
+        <div className="photo">
+            <Image 
             src="/herono.1.jpg"
             alt="Hero illustration"
-            width={400}
-            height={100}
-            className="h-auto w-full rounded-[7.1rem] object-cover"
+            
+            width={200}
+            height={90}
+            className="h-125 w-full rounded-[3.1rem] object-cover"
             />
+            </div>
         </div>
         </div>
-    </div>
+    
     </main>
     </section>
 )
